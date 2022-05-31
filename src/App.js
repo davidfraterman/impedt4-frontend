c267c9f3e8c1fe6f69e390dc6e23a53122ac6ba0
David <davidfraterman@gmail.com>
2022-05-31 15:25:26 +0200

    fetch test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 import Header from './components/UI/Header';
 
 function App() {
+
+  // api call from laravel
+  const laravelApiTest = () => {
+    fetch('localhost:8000/api/test')
+      .then(response => response.json())
+      .then(data => console.log('data', data))
+      .catch(error => console.log(error))
+  }
+  
+  laravelApiTest();
+
+
   return (
     <div className="App">
       <Header />
