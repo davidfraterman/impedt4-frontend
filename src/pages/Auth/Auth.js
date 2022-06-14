1b37f0ee0da3a3042b47545377c92dd4dce43fad
David <davidfraterman@gmail.com>
2022-06-14 10:49:07 +0200

    login design refresh

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,38 +1,38 @@
 import App from "../../App";
 import React from "react";
 
-import profile from './images/pic.png';
 import email from './images/email.png';
 import password from './images/password.png';
+
+import { Icon } from '@iconify/react';
 
 import './login.css';
 
 function Login(){
     return (
-        <div className="main-login">
-            <div className="sub-main-login">
-                <div>
-                    <div className="imgs">
-                            <img  src={profile} alt="profile" className="profile"/>
-                            <h1 className="login-titel">Log in</h1>
+        <section className="main-login">
+            <form className="sub-main-login">
+                <section>
+                    <section className="titleContainer">
+                        <Icon icon="bi:person-fill" color="var(--clr-black)" height="40" />
+                        <h1 className="login-titel">Log in</h1>
+                    </section>
+                </section>
 
-                    </div>
-                </div>
+                <section className="inputSection">
+                    <label htmlFor="email">E-mail</label>
+                    <Icon icon="fluent:mail-16-filled" color="var(--clr-main)" height="22" className="inputIcon" />
+                    <input id="email" type="text" autoComplete="on" placeholder="john@example.com" className="name"/>
+                </section>
+                <section className="inputSection">
+                    <label htmlFor="password">Wachtwoord</label>
+                    <Icon icon="bxs:lock" color="var(--clr-main)" height="22" className="inputIcon" />
+                    <input id="password" type="password" autoComplete="on" className="name"/>
+                </section>
+                <button type="submit" className="login-button">Log in</button>
+            </form>
 
-                <div>
-                    <h1 className="email-titel">E-mail</h1>
-                    <img src={email} alt="email" className="email"/>
-                    <input type="text" placeholder="john@example.com" className="name"/>
-                </div>
-                <div>
-                    <h1 className="email-titel">Wachtwoord</h1>
-                    <img src={password} alt="password" className="password"/>
-                    <input type="password" placeholder="wachtwoord" className="name"/>
-                </div>
-                <button className="login-button">Log in</button>
-            </div>
-
-        </div>
+        </section>
     );
 }
 
