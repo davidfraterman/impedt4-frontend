bc6485dd629297b65f906af67bd23f0b211871f4
David <davidfraterman@gmail.com>
2022-06-15 13:49:44 +0200

    button kaart

--- src/pages/Todo/components/TodoListItem.js
+++ src/pages/Todo/components/TodoListItem.js
@@ -3,15 +3,12 @@ import styles from '../Todo.module.css'
 import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
 
-import * as ROUTES from "../../../constants/routes";
 
 const TodoListItem = (props) => {
   return (
     <tr>
       <td>
-        <Link to={ROUTES.INSPECTIE}>
           <Icon icon="fluent:add-square-24-filled" color="black" height="30" />
-        </Link>
       </td>
       <td>{props.category}</td>
       <td className={styles.tableNumber}>{props.riskindicator}</td>
